13f6abf964d7989fe2c86739e0623422ac26fc80
amandamata <amanda.mata@live.com>
2025-01-01 21:01:19 -0300

    chore: adds investor structs

---
+++ internal/market/entity/investor.go
@@ -0,0 +1,50 @@
+package entity
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+type InvestorAssetPosition struct {
+	AssetID string
+	Shares  int
+}
+
+func NewInvestor(id, name string) *Investor {
+	return &Investor{
+		ID:            id,
+		Name:          name,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func NewInvestorAssetPosition(assetID string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetID,
+		Shares:  shares,
+	}
+}
+
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) UpdateAssetPosition(assetID string, shares int) {
+	assetPosition := i.GetAssetPosition(assetID)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetID, shares))
+	} else {
+		assetPosition.Shares += shares
+	}
+	assetPosition.Shares = shares
+}
+
+func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition {
+	for asset := range i.AssetPosition {
+		if i.AssetPosition[asset].AssetID == assetID {
+			return i.AssetPosition[asset]
+		}
+	}
+	return nil
+}
